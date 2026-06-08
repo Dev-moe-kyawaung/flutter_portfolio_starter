19ba21ddc6c38d80af215a6877599d3f1b6b03ff
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-09 00:12:02 +0630

    Add AppStrings class with application constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,10 @@
+class AppStrings {
+  static const String appName = 'Flutter Portfolio Starter';
+  static const String heroGreeting = 'HELLO, I AM';
+  static const String aboutTitle = 'About Me';
+  static const String servicesTitle = 'Services';
+  static const String projectsTitle = 'Projects';
+  static const String contactTitle = 'Contact';
+  static const String loading = 'Loading...';
+  static const String retry = 'Retry';
+}
