171e431a315c9518148f7466a99f216e610339a9
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-09 00:32:10 +0630

    Add SocialIcon widget for social media interaction

---
+++ lib/core/widgets/social_icon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SocialIcon extends StatelessWidget {
+  const SocialIcon({
+    super.key,
+    required this.icon,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(999),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: const Color(0xFF111A2E),
+          borderRadius: BorderRadius.circular(999),
+          border: Border.all(color: Colors.white.withOpacity(0.08)),
+        ),
+        child: Icon(icon, color: Colors.white70, size: 20),
+      ),
+    );
+  }
+}
