b75d21f49db3fc5fa9542635699e52c8d6742389
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-09 01:13:04 +0630

    Create github_remote_source.dart

---
+++ lib/data/sources/remote/github_remote_source.dart
@@ -0,0 +1,7 @@
+class GitHubRemoteSource {
+  const GitHubRemoteSource();
+
+  Future<Map<String, dynamic>> getUserProfile(String username) async {
+    throw UnimplementedError('Connect GitHub API here later.');
+  }
+}
