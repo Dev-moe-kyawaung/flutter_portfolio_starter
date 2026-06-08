28fa79ce468f522b0bed8cdcaa46f58fbf001563
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-09 00:04:24 +0630

    Create app_assets.dart

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static const String logo = 'assets/images/logo.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String heroBg = 'assets/images/hero_bg.png';
+  static const String projectPlaceholder = 'assets/images/project_placeholder.png';
+}
