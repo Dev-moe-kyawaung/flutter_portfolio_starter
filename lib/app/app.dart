cea3ddcb1691d4a8beb9dcaeda05d9e78762ad6f
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-09 01:18:49 +0630

    Refactor App widget to use ChangeNotifierProvider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
+import 'di/injector.dart';
 import 'router/app_router.dart';
 import 'theme/app_theme.dart';
 
 class App extends StatelessWidget {
-  App({super.key});
-
-  final GoRouter _router = AppRouter.router;
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Portfolio Starter',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
-      routerConfig: _router,
+    return ChangeNotifierProvider(
+      create: (_) => Injector.portfolioState..loadData(),
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Portfolio Starter',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.dark,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
